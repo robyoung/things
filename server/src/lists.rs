2933642581c159c423df3aaea5e751c1e7b5c3a9
Rob Young <rob@robyoung.digital>
2021-12-12 20:42:05 +0000

    Add undo and redo for all ops

--- server/src/lists.rs
+++ server/src/lists.rs
@@ -65,15 +65,18 @@ impl List {
     }
 
     pub fn edit(&mut self, id: u32, value: impl Into<String>) -> Result<ListItem> {
-        let mut item = self
+        let item = self
             .items
             .iter_mut()
             .find(|item| item.id == id)
-            .ok_or(ListError::NotFound)?;
-        let new_value: String = value.into();
-        self.log.push(Operation::edit(&item, &new_value));
-        item.value = new_value;
-        Ok(item.clone())
+            .ok_or(ListError::NotFound)?
+            .clone();
+        let operation = Operation::edit(&item, value);
+
+        self.log.push(operation.clone());
+        self.apply(operation)?;
+
+        Ok(item)
     }
 
     /// Move an item to a position in the list
@@ -85,21 +88,67 @@ impl List {
             .iter()
             .position(|item| item.id == id)
             .ok_or(ListError::NotFound)?;
-
-        let item = self.items.remove(from_i);
-        self.items
-            .insert(cmp::min(position, self.items.len()), item);
+        let operation = Operation::move_to(id, from_i, cmp::min(position, self.items.len() - 1));
+        self.log.push(operation.clone());
+        self.apply(operation)?;
 
         Ok(())
     }
 
     pub fn undo(&mut self) -> Result<()> {
         let operation = self.log.undo()?;
-        self.apply_undo(operation)?;
+        self.revert(operation)?;
         Ok(())
     }
 
-    fn apply_undo(&mut self, operation: Operation) -> Result<()> {
+    pub fn redo(&mut self) -> Result<()> {
+        let operation = self.log.redo()?;
+        self.apply(operation)?;
+        Ok(())
+    }
+
+    fn apply(&mut self, operation: Operation) -> Result<()> {
+        match operation {
+            Operation::Add(item) => {
+                self.items.push(item);
+            }
+            Operation::Remove(_, item) => {
+                self.items.remove(
+                    self.items
+                        .iter()
+                        .position(|itm| itm.id == item.id)
+                        .ok_or(ListError::NotFound)?,
+                );
+            }
+            Operation::Edit(old_item, new_values) => {
+                let mut item = self
+                    .items
+                    .iter_mut()
+                    .find(|item| item.id == old_item.id)
+                    .ok_or(ListError::NotFound)?;
+                if let Some(value) = new_values.value {
+                    item.value = value;
+                }
+            }
+            Operation::MoveTo {
+                id,
+                old_loc: _,
+                new_loc,
+            } => {
+                let from_i = self
+                    .items
+                    .iter()
+                    .position(|item| item.id == id)
+                    .ok_or(ListError::NotFound)?;
+                let item = self.items.remove(from_i);
+                self.items.insert(new_loc, item);
+            }
+            Operation::Root => unreachable!(),
+        }
+        Ok(())
+    }
+
+    fn revert(&mut self, operation: Operation) -> Result<()> {
         match operation {
             Operation::Add(item) => {
                 self.items.remove(
@@ -109,20 +158,31 @@ impl List {
                         .ok_or(ListError::NotFound)?,
                 );
             }
-            Operation::Remove { loc, id, value } => {
-                self.items.insert(loc, ListItem { id, value });
+            Operation::Remove(loc, item) => {
+                self.items.insert(loc, item);
             }
-            Operation::Edit {
-                id,
-                old_value,
-                new_value: _,
-            } => {
+            Operation::Edit(old_item, new_values) => {
                 let mut item = self
                     .items
                     .iter_mut()
-                    .find(|item| item.id == id)
+                    .find(|item| item.id == old_item.id)
                     .ok_or(ListError::NotFound)?;
-                item.value = old_value;
+                if new_values.value.is_some() {
+                    item.value = old_item.value;
+                }
+            }
+            Operation::MoveTo {
+                id,
+                old_loc,
+                new_loc: _,
+            } => {
+                let from_i = self
+                    .items
+                    .iter()
+                    .position(|item| item.id == id)
+                    .ok_or(ListError::NotFound)?;
+                let item = self.items.remove(from_i);
+                self.items.insert(old_loc, item);
             }
             Operation::Root => unreachable!(),
         }
@@ -163,6 +223,8 @@ enum ListError {
     NotFound,
     #[error("Cannot undo")]
     CannotUndo,
+    #[error("Cannot redo")]
+    CannotRedo,
 }
 
 #[derive(Serialize, JsonSchema, Debug, PartialEq, Clone)]
@@ -205,11 +267,24 @@ impl Log {
     }
 
     fn undo(&mut self) -> Result<Operation> {
-        if self.head == 0 {
+        if self.head <= self.fork {
+            // cannot undo beyond what has been committed
             Err(ListError::CannotUndo.into())
         } else {
             let operation = self.records[self.head].operation.clone();
             self.head -= 1;
+            Ok(operation)
+        }
+    }
+
+    // TODO return a reference
+    fn redo(&mut self) -> Result<Operation> {
+        if self.head == self.records.len() - 1 {
+            // cannot redo beyond the end of the log
+            Err(ListError::CannotRedo.into())
+        } else {
+            self.head += 1;
+            let operation = self.records[self.head].operation.clone();
             Ok(operation)
         }
     }
@@ -250,16 +325,18 @@ impl LogRecord {
 enum Operation {
     Root,
     Add(ListItem),
-    Remove {
-        loc: usize,
+    Remove(usize, ListItem),
+    Edit(ListItem, ListItemUpdate),
+    MoveTo {
         id: u32,
-        value: String,
+        old_loc: usize,
+        new_loc: usize,
     },
-    Edit {
-        id: u32,
-        old_value: String,
-        new_value: String,
-    },
+}
+
+#[derive(Clone, Debug, Serialize, JsonSchema)]
+struct ListItemUpdate {
+    value: Option<String>,
 }
 
 impl Operation {
@@ -268,18 +345,23 @@ impl Operation {
     }
 
     pub fn remove(loc: usize, item: ListItem) -> Self {
-        Operation::Remove {
-            loc,
-            id: item.id,
-            value: item.value,
-        }
+        Operation::Remove(loc, item)
     }
 
-    pub fn edit(item: &ListItem, new_value: &str) -> Self {
-        Operation::Edit {
-            id: item.id,
-            old_value: item.value.clone(),
-            new_value: new_value.to_string(),
+    pub fn edit(item: &ListItem, new_value: impl Into<String>) -> Self {
+        Operation::Edit(
+            item.clone(),
+            ListItemUpdate {
+                value: Some(new_value.into()),
+            },
+        )
+    }
+
+    pub fn move_to(id: u32, from_loc: usize, to_loc: usize) -> Self {
+        Self::MoveTo {
+            id,
+            old_loc: from_loc,
+            new_loc: to_loc,
         }
     }
 }
@@ -354,6 +436,31 @@ mod tests {
         }
 
         #[test]
+        fn redo_add() {
+            let mut list = RootList::new("shopping").snapshot();
+            list.add("potatoes");
+            list.add("tomatoes");
+            list.undo().unwrap();
+            list.redo().unwrap();
+            assert_eq!(list_values(&list), vec!["potatoes", "tomatoes"]);
+        }
+
+        #[test]
+        fn redo_beyond_end_fails() {
+            let mut list = RootList::new("shopping").snapshot();
+            list.add("potatoes");
+            list.add("tomatoes");
+            list.undo().unwrap();
+            list.redo().unwrap();
+            let err = list.redo().err().unwrap();
+            assert!(matches!(
+                err.downcast_ref::<ListError>(),
+                Some(&ListError::CannotRedo)
+            ));
+            assert_eq!(list_values(&list), vec!["potatoes", "tomatoes"]);
+        }
+
+        #[test]
         fn undo_remove() {
             let mut list = RootList::new("shopping").snapshot();
             let potatoes = list.add("potatoes");
@@ -365,6 +472,17 @@ mod tests {
         }
 
         #[test]
+        fn redo_remove() {
+            let mut list = RootList::new("shopping").snapshot();
+            let potatoes = list.add("potatoes");
+            list.add("tomatoes");
+            list.remove(potatoes.id);
+            list.undo().unwrap();
+            list.redo().unwrap();
+            assert_eq!(list_values(&list), vec!["tomatoes"]);
+        }
+
+        #[test]
         fn undo_edit() {
             let mut list = RootList::new("shopping").snapshot();
             let potatoes = list.add("potatoes");
@@ -373,6 +491,41 @@ mod tests {
             assert_eq!(list_values(&list), vec!["spuds", "tomatoes"]);
             list.undo().unwrap();
             assert_eq!(list_values(&list), vec!["potatoes", "tomatoes"]);
+        }
+
+        #[test]
+        fn redo_edit() {
+            let mut list = RootList::new("shopping").snapshot();
+            let potatoes = list.add("potatoes");
+            list.add("tomatoes");
+            list.edit(potatoes.id, "spuds").unwrap();
+            list.undo().unwrap();
+            list.redo().unwrap();
+            assert_eq!(list_values(&list), vec!["spuds", "tomatoes"]);
+        }
+
+        #[test]
+        fn undo_move_to() {
+            let mut list = RootList::new("shopping").snapshot();
+            let potatoes = list.add("potatoes");
+            list.add("tomatoes");
+            list.move_to(potatoes.id, 1).unwrap();
+            assert_eq!(list_values(&list), vec!["tomatoes", "potatoes"]);
+            list.undo().unwrap();
+            assert_eq!(list_values(&list), vec!["potatoes", "tomatoes"]);
+        }
+
+        #[test]
+        fn redo_move_to() {
+            let mut list = RootList::new("shopping").snapshot();
+            let potatoes = list.add("potatoes");
+            list.add("tomatoes");
+            list.move_to(potatoes.id, 1).unwrap();
+            assert_eq!(list_values(&list), vec!["tomatoes", "potatoes"]);
+            list.undo().unwrap();
+            assert_eq!(list_values(&list), vec!["potatoes", "tomatoes"]);
+            list.redo().unwrap();
+            assert_eq!(list_values(&list), vec!["tomatoes", "potatoes"]);
         }
     }
 
